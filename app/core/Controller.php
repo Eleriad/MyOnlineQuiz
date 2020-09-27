8d3f5cad73945a3f2bc21c59cf1a36f8b2a5178f
Frédéric Rouffet <frouffet@gmail.com>
2020-09-27 22:35:57 +0200

    Controller.php Creation: display views and models

---
+++ app/core/Controller.php
@@ -0,0 +1,34 @@
+<?php
+
+class Controller
+{
+    /**
+     * Function that returns a model
+     * @return object
+     */
+    protected function model($model)
+    {
+        $modelPath = 'app/models/' . $model . '.php';
+        if (file_exists($modelPath)) {
+            require_once $modelPath;
+            return new $model();
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Function that returns a view
+     * @return object
+     */
+    protected function view($view, $data = [])
+    {
+        $viewPath = 'app/views/' . $view . '.php';
+
+        if (file_exists($viewPath)) {
+            include($viewPath);
+        } else {
+            echo "ERREUR : la vue intitulée $view n'existe pas !"
+        }
+    }
+}
