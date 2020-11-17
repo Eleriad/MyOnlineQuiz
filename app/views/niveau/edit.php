c69b9f9cdbc1cb5e459ff7f62e4ca68e73aa5218
Jkail <boutaultbenoit@yahoo.fr>
2020-11-17 22:36:44 +0100

    niveau controller

--- app/views/niveau/edit.php
+++ app/views/niveau/edit.php
@@ -1,11 +1,11 @@
 <div class="container py-4 text-center">
     <form action="" id="form4" method="post">
         <div class="form-group">
-            <label>Nom de la catégorie : <input type="text" name="categorieName" class="form-control" value="<?= $data->name ?>"></label>
+            <label>Nom du niveau : <input type="text" name="niveauName" class="form-control" value="<?= $data->name ?>"></label>
 
             <div class="text-center mt-3">
-                <input type="submit" name="updateCategorie" value="Modifier cette catégorie" class="btn btn-success">
-                <a href="/categorie/index" class="btn btn-dark">Retour</a>
+                <input type="submit" name="updateNiveau" value="Modifier ce niveau" class="btn btn-success">
+                <a href="/niveau/index" class="btn btn-dark">Retour</a>
             </div>
         </div>
     </form>
