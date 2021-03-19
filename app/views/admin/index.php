e55c509bc0a7f1e1fb4b64399dd1d1b010887b91
Frédéric Rouffet <frouffet@gmail.com>
2021-03-19 16:18:08 +0100

    CSS test on Admin Section's Main Page

--- app/views/admin/index.php
+++ app/views/admin/index.php
@@ -1,12 +1,45 @@
-<h1>Bienvenue sur la page d'accueil Administrateur</h1>
+<div class="container-fluid adminContainer">
 
+    <h1>Bienvenue sur la page d'accueil Administrateur</h1>
 
-<a href="/categorie/index" class="btn btn-info">Gérer les catégories</a>
+    <div class="row">
+        <div class="col-3">
+            <div class="card categoryCard">
+                <i class="fas fa-clipboard-list fa-5x"></i>
+                <div class="card-body">
+                    <a href="/categorie/index" class="adminBtn">Gérer les catégories</a>
+                </div>
+            </div>
+        </div>
 
-<a href="/niveau/index" class="btn btn-info">Gérer les niveaux</a>
+        <div class="col-3">
+            <div class="card levelCard">
+                <i class="fas fa-layer-group fa-5x"></i>
+                <div class="card-body">
+                    <a href="/niveau/index" class="adminBtn">Gérer les niveaux</a>
+                </div>
+            </div>
+        </div>
 
-<a href="/question/index" class="btn btn-info">Gérer les questions</a>
+        <div class="col-3">
+            <div class="card questionCard">
+                <i class="fas fa-question fa-5x"></i>
+                <div class="card-body">
+                    <a href="/question/index" class="adminBtn">Gérer les questions</a>
+                </div>
+            </div>
+        </div>
 
-<a href="/user/index" class="btn btn-info">Gérer les utilisateurs</a>
+        <div class="col-3">
+            <div class="card usersCard">
+                <i class="fas fa-users fa-5x"></i>
+                <div class="card-body">
+                    <a href="/user/index" class="adminBtn">Gérer les utilisateurs</a>
+                </div>
+            </div>
+        </div>
+    </div>
 
-<a href="/quiz/index" class="btn btn-dark">Retour</a>
+    <a href="/quiz/index" class="btn btn-dark btn-lg"><span><i class="far fa-arrow-alt-circle-left mr-2"></i></span>
+        Retour</a>
+</div>
