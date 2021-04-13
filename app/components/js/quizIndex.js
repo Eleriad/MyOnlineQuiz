eb8bb98300cde19e025c9b7e99a147a11311049d
Frédéric Rouffet <frouffet@gmail.com>
2021-04-13 22:34:45 +0200

    test on ajax request

--- app/components/js/quizIndex.js
+++ app/components/js/quizIndex.js
@@ -3,6 +3,8 @@ $(document).ready(function () {
   $(".onChangeLevel").on("change", function () {
     var level = $("#level-select").val();
 
+    var checkButton = document.getElementsByClassName("onChangeCategorie");
+
     $.ajax({
       url: "index",
       type: "post",
@@ -17,11 +19,12 @@ $(document).ready(function () {
 
         getArrayCategories(checkButton);
 
-        // TODO : utiliser le tableau des catégories pour faire un appel AJAX et récupérer le nombre maximal de questions à afficher ensuite
+        // console.log(getArrayCategories(checkButton));
       },
       error: function (status, error) {
         console.log("échec : " + status + error);
       },
+      done: function () {},
     });
   });
 
@@ -29,73 +32,66 @@ $(document).ready(function () {
     $("#divAlert").fadeOut();
   }, 3000);
 
-  // AJAX pour sélection de catégories
-});
+  /**
+   * Function that generate a div with all categories (buttons) related to the choosen level
+   */
+  function generateCategoriesButtons(array) {
+    var categorieDiv = `<div id="initialCat"></div>`;
 
-checkButton = document.getElementsByClassName("onChangeCategorie");
+    $("#levelDiv").after(categorieDiv);
 
-/**
- * Function that generate a div with all categories (buttons) related to the choosen level
- */
-function generateCategoriesButtons(array) {
-  var categorieDiv = `<div id="initialCat"></div>`;
+    for (var i = 0; i < array.length; i++) {
+      let categorieId = parseInt(array[i][0]);
+      let categorieName = array[i][1];
 
-  $("#levelDiv").after(categorieDiv);
+      var categorieLabel = `<label class="btn btn-primary mx-1" for="${categorieId}"><input type="checkbox" id="${categorieId}" value="${categorieId}" name="categories[]" class="onChangeCategorie">${categorieName}</label>`;
 
-  for (var i = 0; i < array.length; i++) {
-    let categorieId = parseInt(array[i][0]);
-    let categorieName = array[i][1];
-
-    var categorieLabel = `<label class="btn btn-primary mx-1" for="${categorieId}"><input type="checkbox" id="${categorieId}" value="${categorieId}" name="categories[]" class="onChangeCategorie">${categorieName}</label>`;
-
-    $("#initialCat").append(categorieLabel);
+      $("#initialCat").append(categorieLabel);
+    }
   }
-}
 
-/**
- * Function that remove the previous div with categorie buttons before generating a new one
- */
-function deleteCategoriesButtons() {
-  removeCatButtons = $("#initialCat");
+  /**
+   * Function that remove the previous div with categorie buttons before generating a new one
+   */
+  function deleteCategoriesButtons() {
+    removeCatButtons = $("#initialCat");
 
-  if (removeCatButtons) {
-    removeCatButtons.remove();
+    if (removeCatButtons) {
+      removeCatButtons.remove();
+    }
   }
-}
 
-function checkCategorie(id) {
-  var checkedCategorie = $("#" + id);
-  checkedCategorie.attr("checked", !checkedCategorie.attr("checked"));
-}
-
-/**
- * Fonction qui récupère les valeurs des catégories sélectionnées
- */
-function getArrayCategories(checkButton) {
   // variable du tableau de catégories
   var categorieArray = [];
 
-  // on parcourt tous les boutons
-  for (let i = 0; i < checkButton.length; i++) {
-    // Quand on clique sur un bouton
-    checkButton[i].addEventListener("click", function () {
-      // ON récupère sa valeur
-      var value = checkButton[i].value;
+  /**
+   * Fonction qui récupère les valeurs des catégories sélectionnées
+   */
+  function getArrayCategories(checkButton) {
+    // on parcourt tous les boutons
+    for (let i = 0; i < checkButton.length; i++) {
+      // Quand on clique sur un bouton
+      checkButton[i].addEventListener("click", function () {
+        // On récupère sa valeur
+        var value = checkButton[i].value;
 
-      // Si le bouton est coché
-      if (checkButton[i].checked) {
-        // ON insère sa valeur dans le tableau des catégories
-        categorieArray.push(value);
-      } else {
-        // Si le bouton est décoché et que la valeur est déjà dans le tableau des catégories
-        if (categorieArray.includes(value)) {
-          // On récpuère sa position dans le tableau
-          valueIndex = categorieArray.indexOf(value);
-          // on supprime la valeur du tableau
-          categorieArray.splice(valueIndex, 1);
+        // Si le bouton est coché
+        if (checkButton[i].checked) {
+          // On insère sa valeur dans le tableau des catégories
+          categorieArray.push(value);
+        } else {
+          // Si le bouton est décoché et que la valeur est déjà dans le tableau des catégories
+          if (categorieArray.includes(value)) {
+            // On récpuère sa position dans le tableau
+            valueIndex = categorieArray.indexOf(value);
+            // on supprime la valeur du tableau
+            categorieArray.splice(valueIndex, 1);
+          }
         }
-      }
-      return categorieArray;
-    });
+        console.log(categorieArray);
+        return categorieArray;
+        // TODO : utiliser le tableau des catégories pour faire un appel AJAX et récupérer le nombre maximal de questions à afficher ensuite
+      });
+    }
   }
-}
+});
