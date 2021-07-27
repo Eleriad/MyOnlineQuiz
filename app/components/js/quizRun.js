67481ca20e394adfb6fa320d4f19e8c847a16e22
Frédéric Rouffet <frouffet@gmail.com>
2021-07-27 22:33:52 +0200

    displaying all questions OK with JQuery

--- app/components/js/quizRun.js
+++ app/components/js/quizRun.js
@@ -3,52 +3,38 @@ $(document).ready(function () {
   let userAnswer = []; // tableau qui va accueillir les réponses de l'utilisateur
   let questionNb = 1; // nombre de départ des questions
   let maxQuestion = $('input[name="maxQuestion"]').val(); // récupération du nombre maximal de questions
+  let count = $(".questionTitle").length;
+  let button = $("[id^='questionDiv']").find(".btnNext"); // selection of the btnNext button
+  button.attr("disabled", "disabled"); // adding the disabled attribute to the button in order to prevent people from validates
 
-  // for (i = questionNb; i < maxQuestion; i++) {
-  // When clicking on any radio button, add the selectedAnswer Class to parent div
-  $("[name^='radio_']").click(function () {
+  $(".cont").hide(); // cache par défaut toutes les questions
+  $("#questionDiv" + 1).show();
+
+  // Quand on clique sur un bouton radio, When clicking on any radio button, add the selectedAnswer Class to parent div
+  $("[name^='radio']").click(function () {
     if ($(this).is(":checked")) {
       self = $(this).parent();
 
       selectedAnswer = $(".answersDiv").find("div.selectedAnswer");
       selectedAnswer.removeClass("selectedAnswer");
-      let button = $("[class^='questionDiv']").find("[id^='confirmBtn']");
-      button.removeClass("disabled");
-      console.log(button);
-      // console.log("#confirmBtn_" + i);
-
-      // TODO : vérifier que cela n'enlève la classe disabled qu'au bouton de la question en cours ("#confirmBtn_" + i ne fonctionne pas et .BtnNext les disabled tous)
+      button.removeAttr("disabled"); // on enlève l'attribut "disabled"
+      button.addClass("validate"); // on remet
 
       self.toggleClass("selectedAnswer");
     }
   });
 
-  // When clicking on button that has confirmBtn's id, add the user answer to the userAnswer array
-  // $("#confirmBtn_" + i).click(function (e) {
-  //   e.preventDefault();
-  //   let answer = $(".selectedAnswer :input").val();
-  //   userAnswer.push(answer);
-  //   console.log(userAnswer);
-  // });
-  // }
+  // Quand on clique sur le bouton pour passer à la question suivante
+  $(document).on("click", ".btnNext", function (e) {
+    e.preventDefault();
+    last = parseInt($(this).attr("id")); // on récupère l'id qui correspond au numéro de la question
+    next = last + 1; // on définit le numéro dela question suivante
+    $("#questionDiv" + last).hide(); // on cache la question qu'on vient de faire
+    $("#questionDiv" + next).show(); // on affiche la suivante
+    button.attr("disabled", "disabled"); // on remet le bouton de validation en disabled
+    button.removeClass("validate"); // on enlève la classe "validate" qui active le bouton
+  });
 });
-
-// TODO : voir comment récupérer toutes les div sauf celle de la question en cours
-// let questionNb = 1;
-// displayCurrentQuestion(questionNb);
-
-// function displayCurrentQuestion(nb) {
-// $('div[class^="questionDiv"]').siblings().parent().css('display', 'none');
-// $('.questionDiv_' + nb).show();
-
-// }
 
 // TODO : on click sur le bouton : cela permet d'enregistrer la réponse de l'utilisateur + cache la question précédente et affiche la question suivante
 // (sauf si dernière question, dans ce cas, on renvoie sur la page de feedback)
-
-// TODO : on cache ici les questions qui ne sont pas celle en cours
-// $('.questionDiv_1').parent().css('display', 'none');
-// $('.questionDiv_2').parent().css('display', 'none');
-// $('.questionDiv_3').parent().css('display', 'none');
-// $('.questionDiv_4').parent().css('display', 'none');
-// $('.questionDiv_5').parent().css('display', 'none');
--- app/views/quiz/quiz.php
+++ app/views/quiz/quiz.php
@@ -1,113 +1,89 @@
-<?php
-$currentQuestion = $_SESSION["currentQuestion"];
-$categorieName = "";
+<div class="container-fluid quizContainer">
+    <?php
+    $currentQuestion = $_SESSION["currentQuestion"];
+    $categorieName = "";
 
-// var_dump($data);
-// var_dump($_SESSION);
+    foreach ($data["categorieName"] as $test) {
+        $test2 = implode($test);
+        $categorieName .= $test2 . ", ";
+    }
 
-foreach ($data["categorieName"] as $test) {
-    $test2 = implode($test);
-    $categorieName .= $test2 . ", ";
-}
+    $categorieName = rtrim($categorieName, ", ");
+    $questionInt = 1;
+    $correctAnswers = [];
 
-$categorieName = rtrim($categorieName, ", ");
-$questionInt = 1;
-?>
-<?php
-$correctAnswers = [];
-foreach ($data["questions"] as $question) :
-    $currentQuestion++;
-    $correctAnswers[$currentQuestion] = $question["reponse"];
+    foreach ($data["questions"] as $question) :
+        $currentQuestion++;
+        $correctAnswers[$currentQuestion] = $question["reponse"];
+        $choix = array_slice($question, 2);
+        $shuffle = shuffle($choix);
+    ?>
 
+    <!-- Input hidden pour récupérer en JS le nombre maximum de questions -->
+    <input type="hidden" name="maxQuestion" value="<?= $_SESSION["questionNb"] ?>">
+    <form action="/quiz/results" method="POST">
+        <div class="quizDiv">
 
-    $choix = array_slice($question, 2);
-    $shuffle = shuffle($choix);
-?>
+            <!-- Input hidden, pour récupérer en AJAX le numéro de la question en cours -->
+            <input type="hidden" name="currentQuestion" value="<?= $currentQuestion ?>">
 
-<!-- <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-    <div class="carousel-inner">
-        <div class="carousel-item <?php //echo $questionInt = 1 ? "active" : "" 
-                                    ?>"> -->
-<!-- Input hidden pour récupérer en JS le nombre maximum de questions -->
-<input type="hidden" name="maxQuestion" value="<?= $_SESSION["questionNb"] ?>">
-<form action="/quiz/results" method="POST">
-    <div class="quizDiv">
+            <!-- Input hidden pour récupérer un array de toutes les bonnes réponses en php -->
+            <?php foreach ($correctAnswers as $test) : ?>
+            <input type="hidden" name="result[]" value="<?= $test ?>">
+            <?php endforeach ?>
 
-        <!-- Input hidden, pour récupérer en AJAX le numéro de la question en cours -->
-        <input type="hidden" name="currentQuestion" value="<?= $currentQuestion ?>">
+            <div id="questionDiv<?= $currentQuestion ?>" class="cont">
 
-        <!-- Input hidden pour récupérer un array de toutes les bonnes réponses en php -->
-        <?php foreach ($correctAnswers as $test) : ?>
-        <input type="hidden" name="result[]" value="<?= $test ?>">
-        <?php endforeach ?>
-
-        <div class="questionDiv_<?= $currentQuestion ?>">
-
-            <!-- TODO : mettre en forme les données en haut du quiz + l'affichage des choix de réponse -->
-            <div class="quizTitle text-center">
-                <p>Quizz <i><?= $categorieName ?></i>, niveau <i><?= $data["levelName"]["level"] ?></i>
-                    <br>
-                <p class="subTitle">
-                    <?php
-                        echo $questionInt == $_SESSION["questionNb"] ? "Dernière question" : 'Question <strong>' . $questionInt . '</strong> sur <strong>' . $_SESSION["questionNb"] . '</strong>';
-                        ?>
-                </p>
-                </p>
-            </div>
-            <div id="<?= $currentQuestion ?>" class="questionTitle">
-                <?= $question["question"] ?>
-                <hr class="divider">
-            </div>
-            <div class="answersDiv">
-                <div class="d-inline">
-                    <div class="answer">
-                        <input type="radio" id="answer1_<?= $currentQuestion ?>" name="radio_<?= $currentQuestion ?>"
-                            value="<?= $choix[0] ?>" class="answer1 mr-2">
-                        <label for="answer1_<?= $currentQuestion ?>"><?= $choix[0] ?></label>
+                <!-- TODO : mettre en forme les données en haut du quiz + l'affichage des choix de réponse -->
+                <div class="quizTitle text-center">
+                    <p>Quizz <i><?= $categorieName ?></i>, niveau <i><?= $data["levelName"]["level"] ?></i><br>
+                    <p class="subTitle">
+                        <?php
+                            echo $questionInt == $_SESSION["questionNb"] ? "Dernière question" : 'Question <strong>' . $questionInt . '</strong> sur <strong>' . $_SESSION["questionNb"] . '</strong>';
+                            ?>
+                    </p>
+                    </p>
+                </div>
+                <div id="<?= $currentQuestion ?>" class="questionTitle">
+                    <?= $question["question"] ?>
+                    <hr class="divider">
+                </div>
+                <div class="answersDiv">
+                    <div class="d-inline">
+                        <div class="answer">
+                            <input type="radio" id="answer1_<?= $currentQuestion ?>" name="radio<?= $currentQuestion ?>"
+                                value="<?= $choix[0] ?>" class="answer1 mr-2">
+                            <label for="answer1_<?= $currentQuestion ?>"><?= $choix[0] ?></label>
+                        </div>
+                        <div class="answer">
+                            <input type="radio" id="answer2_<?= $currentQuestion ?>" name="radio<?= $currentQuestion ?>"
+                                value="<?= $choix[1] ?>" class="answer2 mr-2">
+                            <label for="answer2_<?= $currentQuestion ?>"><?= $choix[1] ?></label>
+                        </div>
                     </div>
-                    <div class="answer">
-                        <input type="radio" id="answer2_<?= $currentQuestion ?>" name="radio_<?= $currentQuestion ?>"
-                            value="<?= $choix[1] ?>" class="answer2 mr-2">
-                        <label for="answer2_<?= $currentQuestion ?>"><?= $choix[1] ?></label>
+                    <div class="d-inline">
+                        <div class="answer">
+                            <input type="radio" id="answer3_<?= $currentQuestion ?>" name="radio<?= $currentQuestion ?>"
+                                value="<?= $choix[2] ?>" class="answer3 mr-2">
+                            <label for="answer3_<?= $currentQuestion ?>"><?= $choix[2] ?></label>
+                        </div>
+                        <div class="answer">
+                            <input type="radio" id="answer4_<?= $currentQuestion ?>" name="radio<?= $currentQuestion ?>"
+                                value="<?= $choix[3] ?>" class="answer4 mr-2">
+                            <label for="answer4_<?= $currentQuestion ?>"><?= $choix[3] ?></label>
+                        </div>
                     </div>
                 </div>
-                <div class="d-inline">
-                    <div class="answer">
-                        <input type="radio" id="answer3_<?= $currentQuestion ?>" name="radio_<?= $currentQuestion ?>"
-                            value="<?= $choix[2] ?>" class="answer3 mr-2">
-                        <label for="answer3_<?= $currentQuestion ?>"><?= $choix[2] ?></label>
-                    </div>
-                    <div class="answer">
-                        <input type="radio" id="answer4_<?= $currentQuestion ?>" name="radio_<?= $currentQuestion ?>"
-                            value="<?= $choix[3] ?>" class="answer4 mr-2">
-                        <label for="answer4_<?= $currentQuestion ?>"><?= $choix[3] ?></label>
-                    </div>
-                </div>
+                <button id="<?php echo $questionInt == $_SESSION["questionNb"] ? "endQuizBtn" : "$currentQuestion"; ?>"
+                    class="mt-5 <?php echo $questionInt == $_SESSION["questionNb"] ? "endQuizBtn" : "btnNext"; ?>"><?php echo $questionInt == $_SESSION["questionNb"] ? "Fin du quizz" : "Question suivante"; ?></button>
             </div>
-            <button
-                id="<?php echo $questionInt == $_SESSION["questionNb"] ? "endQuizBtn" : "confirmBtn_$currentQuestion"; ?>"
-                class="mt-5 btnNext disabled"><?php echo $questionInt == $_SESSION["questionNb"] ? "Fin du quizz" : "Question suivante"; ?></button>
         </div>
-    </div>
-</form>
-<!-- </div> -->
-<?php
-    $questionInt++;
-endforeach;
-// var_dump($correctAnswers);
-?>
-<!-- <div class="carousel-item">
-            <p>toto</p>
-        </div>
-    </div>
-    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
-</div> -->
+    </form>
+
+    <?php
+        $questionInt++;
+    endforeach;
+    ?>
+</div>
 
 <script src="/app/components/js/quizRun.js"></script>
